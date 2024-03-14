522cd9de5786274eb7aff87872eb16a1a278be34
sabbir073 <md.sabbir073@gmail.com>
2024-03-14 22:50:10 +0600

    wildcard added cors

--- next.config.mjs
+++ next.config.mjs
@@ -7,7 +7,7 @@ const nextConfig = {
                 source: "/api/:path*",
                 headers: [
                     { key: "Access-Control-Allow-Credentials", value: "true" },
-                    { key: "Access-Control-Allow-Origin", value: "https://eisomoy-dashboard-node.vercel.app, https://ei-matro.vercel.app" }, // replace this with your actual origin
+                    { key: "Access-Control-Allow-Origin", value: process.env.ALLOWED_ORIGIN }, // replace this with your actual origin
                     { key: "Access-Control-Allow-Methods", value: "GET,DELETE,PATCH,POST,PUT,OPTIONS" },
                     { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
                 ]
