de065ad3cd3e0d8c38ef4cba26c40c5309b7458c
sabbir073 <md.sabbir073@gmail.com>
2024-03-15 22:05:46 +0600

    delete news added

---
+++ pages/api/news/delete-news.js
@@ -0,0 +1,78 @@
+import { connectToDatabase } from '../../../db';
+import jwt from 'jsonwebtoken';
+
+export default async function handler(req, res) {
+    // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+  // Set Access-Control-Allow-Origin header dynamically based on the request origin
+  const origin = req.headers.origin;
+  const allowedOrigins = ['https://eisomoy-dashboard-node.vercel.app', 'https://ei-matro.vercel.app', 'https://ei-matro-dusky.vercel.app'];
+  
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    // If the origin is not allowed, return a CORS error response
+    res.status(403).json({ error: 'Origin not allowed' });
+    return;
+  }
+
+    // Handle preflight request
+    if (req.method === 'OPTIONS') {
+        res.status(200).end(); // Respond with 200 status code for preflight requests
+        return;
+    }
+
+    if (req.method === 'POST') { // Change method check to POST
+        try {
+            // Parse token from request cookies
+            const token = req.cookies.token;
+
+            if (!token) {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+
+            // Verify token
+            const decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+
+            // Extract user's role
+            const userRole = decodedToken.role;
+
+            // Check if the user has the necessary role to delete users
+            if (userRole !== 'admin' && userRole !== 'editor' && userRole !== 'reporter') {
+                return res.status(403).json({ message: 'Forbidden' });
+            }
+
+            // Extract the userid from the request body
+            const { cat_id } = req.body; // Assuming the user ID is sent in the request body
+
+            if (!cat_id) {
+                return res.status(400).json({ message: 'Something is wrong' });
+            }
+
+            const db = await connectToDatabase();
+
+            // Check if the user exists
+            const existingUser = await db.collection('news').findOne({ _id: cat_id });
+
+            if (!existingUser) {
+                return res.status(404).json({ message: 'News not found' });
+            }
+
+            // Delete the user
+            await db.collection('news').deleteOne({ _id: cat_id });
+
+            res.status(200).json({ message: 'News deleted successfully' });
+        } catch (error) {
+            console.error(error);
+            if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+                return res.status(401).json({ message: 'Unauthorized' });
+            }
+            res.status(500).json({ message: 'Server Error' });
+        }
+    } else {
+        res.status(405).json({ message: 'Method Not Allowed' });
+    }
+}
