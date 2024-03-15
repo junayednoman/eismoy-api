7f55d4e5871aa8d2cb6c894a47f422acd50a7db6
sabbir073 <md.sabbir073@gmail.com>
2024-03-16 01:49:32 +0600

    scroll done

---
+++ pages/api/scroll/scroll-front.js
@@ -0,0 +1,51 @@
+// pages/api/all-users.js
+import { connectToDatabase } from '../../../db';
+import jwt from 'jsonwebtoken';
+
+export default async function handler(req, res) {
+
+  // Set CORS headers
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
+  res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
+  
+  // Set Access-Control-Allow-Origin header dynamically based on the request origin
+  const origin = req.headers.origin;
+  const allowedOrigins = ['https://eisomoy-dashboard-node.vercel.app', 'https://ei-matro.vercel.app', 'https://ei-matro-dusky.vercel.app'];
+  
+  if (origin && allowedOrigins.includes(origin)) {
+    res.setHeader('Access-Control-Allow-Origin', origin);
+  } else {
+    // If the origin is not allowed, return a CORS error response
+    res.status(403).json({ error: 'Origin not allowed' });
+    return;
+  }
+
+  // Handle preflight request
+  if (req.method === 'OPTIONS') {
+    res.status(200).end(); // Respond with 200 status code for preflight requests
+    return;
+  }
+  
+  if (req.method === 'GET') {
+
+    try {
+      
+
+     // Fetch all user details from the database where scroll_status is "on"
+        const db = await connectToDatabase();
+        const categories = await db.collection('scroll').find({ scroll_status: 'on' }, { projection: { _id: 0, scroll_id: 0 } }).toArray(); // Exclude _id, password, and forget_password_token fields
+
+      
+      res.status(200).json(categories);
+    } catch (error) {
+      console.error(error);
+      if (error.name === 'JsonWebTokenError' || error.name === 'TokenExpiredError') {
+        return res.status(401).json({ message: 'Unauthorized' });
+      }
+      res.status(500).json({ message: 'Server Error' });
+    }
+  } else {
+    res.status(405).json({ message: 'Method Not Allowed' });
+  }
+}
