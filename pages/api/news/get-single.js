4a646e2d3390594956077decc3056b1d386c8ac3
sabbir073 <md.sabbir073@gmail.com>
2024-03-16 12:39:37 +0600

    changed to post

--- pages/api/news/get-single.js
+++ pages/api/news/get-single.js
@@ -25,7 +25,7 @@ export default async function handler(req, res) {
         return;
     }
 
-    if (req.method === 'GET') {
+    if (req.method === 'POST') {
         try {
             // Extract the cat_id from the request query parameters
             const { newsid } = req.body;
