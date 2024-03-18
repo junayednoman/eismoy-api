3871d27fd673f749ee9b2b517b09262041f1db1c
junayednoman <junayednoman05@gmail.com>
2024-03-18 16:03:47 +0600

    api updated

--- pages/api/ads/get-ad.js
+++ pages/api/ads/get-ad.js
@@ -1,15 +1,16 @@
 import { connectToDatabase } from '../../../db';
+import { ObjectId } from 'mongodb';
 
 export default async function handler(req, res) {
     // Set CORS headers
     res.setHeader('Access-Control-Allow-Credentials', true);
     res.setHeader('Access-Control-Allow-Methods', 'GET,OPTIONS,PATCH,DELETE,POST,PUT');
     res.setHeader('Access-Control-Allow-Headers', 'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version');
-
+  
     // Set Access-Control-Allow-Origin header dynamically based on the request origin
     const origin = req.headers.origin;
-    const allowedOrigins = ['https://eisomoy-dashboard-node.vercel.app', 'https://ei-matro.vercel.app', 'https://ei-matro-dusky.vercel.app', 'http://localhost:3000'];
-
+    const allowedOrigins = ['https://eisomoy-dashboard-node.vercel.app', 'https://ei-matro.vercel.app', 'https://eimattro.com', 'http://localhost:3000'];
+  
     if (origin && allowedOrigins.includes(origin)) {
         res.setHeader('Access-Control-Allow-Origin', origin);
     } else {
@@ -18,7 +19,6 @@ export default async function handler(req, res) {
         return;
     }
 
-    
     // Handle preflight request
     if (req.method === 'OPTIONS') {
         res.status(200).end(); // Respond with 200 status code for preflight requests
@@ -27,27 +27,23 @@ export default async function handler(req, res) {
 
     if (req.method === 'POST') {
         try {
-            const { ad_name } = req.body; // Include ad_name in the request body
+            // Extract the ad name from the request body
+            const { ad_name } = req.body;
 
-            // Check if ad_name is provided
             if (!ad_name) {
-                res.status(400).json({ message: 'Ad name is required' });
-                return;
+                return res.status(400).json({ message: 'ad_name is required' });
             }
 
-            // Fetch the ad from the database based on the provided ad name
             const db = await connectToDatabase();
 
-            const query = { ad_name: ad_name, status: "Show" };
+            // Find the current ad item by its ad_name and status
+            const currentAdItem = await db.collection('ads').findOne({ ad_name: ad_name, status: "Show" });
 
-            // Fetch the ad based on the query
-            const ad = await db.collection('ads')
-                .findOne(query);
-            if (ad) {
-                res.status(200).json(ad);
-            } else {
-                res.status(404).json({ message: 'Ad not found' });
+            if (!currentAdItem) {
+                return res.status(404).json({ message: 'Ad not found' });
             }
+
+            res.status(200).json({ currentAdItem });
         } catch (error) {
             console.error(error);
             res.status(500).json({ message: 'Server Error' });
